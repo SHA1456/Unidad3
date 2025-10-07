a4b4a38a52b948fdc8b0fa8203bcab2ac82666a2
SHA1456 <heredia.acero.sergio@iescamas.es>
2025-10-07 21:03:22 +0200

    Add files via upload

---
+++ Act6.java
@@ -0,0 +1,20 @@
+package com;
+import java.util.Scanner;
+public class Act6 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int i;
+		
+		for(i=7;i<100;i=i+7) {
+			
+			System.out.println(i);
+			
+		}
+		
+		
+		sc.close();
+	}
+
+}
